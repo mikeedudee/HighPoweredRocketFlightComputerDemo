7d5ee658a8e4e0ccae8be33c6b51dc713df21163
Francis Mike John Camogao <153460484+mikeedudee@users.noreply.github.com>
2025-06-28 20:09:42 +0800

    Update FlashFlush.ino

--- FlashFlush.ino
+++ FlashFlush.ino
@@ -58,17 +58,3 @@ void addToBuffer(const DataPoint &pt) {
     bufferArray[BUFFER_SIZE-1] = pt;
   }
 }
-
-/// Blink the red LED at a fixed period to indicate a critical error.
-/// This function does not return.
-static void errorBlink() {
-    // Blink forever: 500 ms on, 500 ms off
-    for (;;)
-    {
-        digitalWrite(PIN_RED_LED, HIGH);
-        delay(500);
-
-        digitalWrite(PIN_RED_LED, LOW);
-        delay(500);
-    }
-}
